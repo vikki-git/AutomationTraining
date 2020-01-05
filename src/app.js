89a8f5b7441122763ef4134d8dcbffdb4eb257cf
“vikki-git” <vikusbka@gmail.com>
2020-01-05 04:42:04 +0200

    app spec updated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
-// import {userData} from "./users";
 const http = require('http');
-const process = require('process');
-// const userData = require("./users");
 
 const app = http.createServer((req, res) => {
     console.log(req.method);
-    // res.write(userData);
     res.write('dsfsd');
     res.end();
     // process.exit(1);
