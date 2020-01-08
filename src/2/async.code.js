15f0452ceaf63eeda760fb41b64047b5ce223925
“vikki-git” <vikusbka@gmail.com>
2020-01-08 05:20:43 +0200

    app2 spec updated

--- src/2/async.code.js
+++ src/2/async.code.js
@@ -1,22 +1,15 @@
-const fetchData = () => {
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Done!');
-        }, 1500);
-    });
-    return promise;
+const fetchData = (callback) => {
+    setTimeout(() => {
+        callback('Done!');
+    }, 1500);
 };
 
 setTimeout(() => {
     console.log('Timer is done!');
-    fetchData()
-        .then(text => {
-            console.log(text);
-            return fetchData();
-        })
-        .then(text2 => {
-            console.log(text2);
-        });
+    fetchData(text => {
+        console.log(text);
+    })
+
 }, 2000);
 
 console.log('Hello!');
