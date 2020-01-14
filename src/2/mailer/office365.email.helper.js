6be047cc9ef02758e98df6bd36917a170a336572
viktoriia-mykhailova <viktoriia.mykhailova@drillinginfo.com>
2020-01-14 21:39:23 +0200

    sendinblue added

--- src/2/mailer/office365.email.helper.js
+++ src/2/mailer/office365.email.helper.js
@@ -1,42 +1,48 @@
 const nodemailer = require('nodemailer');
 
-const sendEmailData = {
+// const sendEmail = async () => {
+//        await nodemailer.createTransport(
+//             {
+//                 host: 'smtp.office365.com',
+//                 port: 587,
+//                 auth: {
+//                     user: 'viktoriia.mykhailova@drillinginfo.com',
+//                     pass: 'Apelsin#1025',
+//                 },
+//                 secureConnection: false,
+//                 tls: {
+//                     ciphers: 'SSLv3',
+//                     rejectUnauthorized: false,
+//                 },
+//             }
+//         ).sendMail({
+//             to: 'mihailova.vika@gmail.com',
+//             from: 'viktoriia.mykhailova@drillinginfo.com',
+//             subject: 'mail sent!',
+//             text: 'email test',
+//         });
+// };
+//
+// sendEmail()
+//     .then(resp => console.log(resp), err => console.log(err));
+
+nodemailer.createTransport(
+    {
+        host: 'smtp.office365.com',
+        port: 587,
+        auth: {
+            user: 'viktoriia.mykhailova@drillinginfo.com',
+            pass: 'Apelsin#1025',
+        },
+        secureConnection: false,
+        tls: {
+            ciphers: 'SSLv3',
+            rejectUnauthorized: false,
+        },
+    }
+).sendMail({
     to: 'mihailova.vika@gmail.com',
     from: 'viktoriia.mykhailova@drillinginfo.com',
     subject: 'mail sent!',
     text: 'email test',
-};
-
-const sendEmail = ({sendEmailData}) => {
-    return new Promise((resolve, reject) => {
-        nodemailer.createTransport(
-            {
-                host: 'smtp.office365.com',
-                port: 587,
-                auth: {
-                    user: 'viktoriia.mykhailova@drillinginfo.com',
-                    pass: 'Apelsin#1025',
-                },
-                secureConnection: false,
-                tls: {
-                    ciphers: 'SSLv3',
-                    rejectUnauthorized: false,
-                },
-            }
-        ).sendMail({
-            to: 'mihailova.vika@gmail.com',
-            from: 'viktoriia.mykhailova@drillinginfo.com',
-            subject: 'mail sent!',
-            text: 'email test',
-        }, async (error, info) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(info);
-            }
-        });
-    });
-};
-
-sendEmail(sendEmailData)
-    .then(resp => console.log(resp), err => console.log(err));
+}).then(resp => console.log(resp), err => console.log(err));
---
+++ src/2/mailer/office365.email.helper.v1.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+nodemailer.createTransport(
+    {
+        host: 'smtp.office365.com',
+        port: 587,
+        auth: {
+            user: 'viktoriia.mykhailova@drillinginfo.com',
+            pass: 'Apelsin#1025',
+        },
+        secureConnection: false,
+        tls: {
+            ciphers: 'SSLv3',
+            rejectUnauthorized: false,
+        },
+    }
+).sendMail({
+    to: 'mihailova.vika@gmail.com',
+    from: 'viktoriia.mykhailova@drillinginfo.com',
+    subject: 'mail sent!',
+    text: 'email test',
+}).then(resp => console.log(resp), err => console.log(err));
